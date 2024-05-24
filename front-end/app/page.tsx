387bf92511adcf410241ed91064840a7a98f8eec
karanza5 <karan44107@mail.com>
2024-05-24 10:22:36 +0530

    css styles

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,9 +1,16 @@
+import { Content } from "next/font/google";
 import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/footer/Footer";
+import Content1 from "./components/content/Content";
 
 export default function Home() {
   return (
   <>
   <Header />
+  <Content1 />
+  <Hero />
+  <Footer />
   </>
   );
 }
